bffd7f0f1436d32c35d7c24cd12af9645d22f5c9
RoliVegas <roland.krancz@gmail.com>
2022-03-17 08:34:38 +0100

    Additional routings

--- src/App.js
+++ src/App.js
@@ -1,13 +1,38 @@
 import React from 'react';
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, Outlet } from 'react-router-dom';
 
 import Home from './routes/home/home.component';
 import './App.css';
 
+const Navigation = () => {
+    return (
+        <div>
+            <div>
+                <h1>Navigation bar</h1>
+            </div>
+            <Outlet />
+        </div>
+    )
+}
+
+const Shop = () => {
+    return (
+        <div>
+            <div>
+                <h1>Shop</h1>
+            </div>
+        </div>
+    )
+}
+
 const App = () => {
     return (
         <Routes>
-            <Route path='/' element={<Home />} />
+            <Route path='/' element={<Navigation />}>
+                {/* index{true} == render if the parent element ('/') is matched. */ } 
+                <Route index element={<Home />} />
+                <Route path='shop' element={<Shop />} />
+            </Route>
         </Routes>
     );
 };
