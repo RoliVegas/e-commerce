1940ce852055188c4213973708a5b95a9434639f
RoliVegas <roland.krancz@gmail.com>
2022-05-08 18:45:17 +0200

    Added signin with redirect

--- src/routes/sign-in/sign-in.component.jsx
+++ src/routes/sign-in/sign-in.component.jsx
@@ -1,15 +1,35 @@
-import React from 'react'
-import { signInWithGooglePopup, createUserDocumentFromAuth } from '../../utils/firebase/firebase.utils.js';
+import {React, useEffect} from 'react'
+import { getRedirectResult } from 'firebase/auth';
+
+import { auth, 
+         signInWithGooglePopup, 
+         signInWithGoogleRedirect,    
+         createUserDocumentFromAuth } from '../../utils/firebase/firebase.utils.js';
 
 const SignIn = () => {
+
+    useEffect(async () => {
+        const response = await getRedirectResult(auth);
+        if(response) {
+            const userDocRef = await createUserDocumentFromAuth(response.user);
+        }
+    }, []);
+
     const logGoogleUser = async () => {
         const { user } = await signInWithGooglePopup();
         createUserDocumentFromAuth(user);
     }
+    const logGoogleUserRedirect = async () => {
+        const { user } = await signInWithGoogleRedirect();
+        createUserDocumentFromAuth(user);
+    }
+
     return (
         <div>
             <h1>Sign In Page</h1>
             <button onClick={logGoogleUser}>Sign in with Google Popup</button>
+            <button onClick={logGoogleUserRedirect}>Sign in with Google Redirect</button>
+
         </div>
   )
 }
--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -30,8 +30,29 @@ provider.setCustomParameters({
 });
 
 export const auth = getAuth();
-export const signInWithGooglePopup = () => signInWithPopup(auth, provider);
 export const db = getFirestore();
+
+export const signInWithGooglePopup = () => signInWithPopup(auth, provider);
+export const signInWithGoogleRedirect = () => signInWithRedirect(auth, provider);
+
 export const createUserDocumentFromAuth = async (userAuth) => {
     const userDocRef = doc(db, 'users', userAuth.uid);
-}
+    const userSnapshot = await getDoc(userDocRef);
+
+    if(!userSnapshot.exists()) {
+        const { displayName, email } = userAuth;
+        const createdAt = new Date();
+
+        try {
+            await setDoc(userDocRef, {
+                displayName,
+                email,
+                createdAt
+            });
+        } catch(error) {
+            console.log('Error creating the user', error.message);
+        }
+    }
+
+    return userDocRef;
+};
